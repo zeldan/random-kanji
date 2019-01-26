32aaafcd0bf48925f20f06eaad05eaedcfe1d4a7
DanielZelei <zelei.dan@gmail.com>
2019-01-26 22:48:28 +0100

    deploy to proper directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -x
 
-scp ./target/com.randomkanji-0.0.1-SNAPSHOT.jar $REMOTE_USER@$REMOTE_HOST
+scp ./target/com.randomkanji-0.0.1-SNAPSHOT.jar $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR
